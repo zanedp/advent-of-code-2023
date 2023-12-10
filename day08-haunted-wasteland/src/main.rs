6c561d5aace71f6eb00ac4c060b6961eef4c5acc
Zane D. Purvis <zanedp@users.noreply.github.com>
2023-12-10 14:13:06 -0500

    Switch to using LCM

--- day08-haunted-wasteland/src/main.rs
+++ day08-haunted-wasteland/src/main.rs
@@ -45,41 +45,72 @@ fn part1() {
 
 fn part2() {
     // let (input, expected_steps) = (include_str!("sample2.txt"), Some(6));
-    // let (input, expected_steps) = (include_str!("sample1-2.txt"), Some(6));
-    let (input, expected_steps) = (include_str!("my_input.txt"), None::<u64>);
+    let (input, expected_steps) = (include_str!("my_input.txt"), Some(8811050362409_u64));
     let (instructions, graph) = parse_input(input);
 
-    let mut cur_nodes = graph
+    let start_nodes = graph
         .iter()
         .filter(|(node, _)| node.ends_with('A'))
         .map(|(node, _)| node)
         .collect::<Vec<_>>();
-    // dbg!(&cur_nodes);
 
-    let mut num_steps = 0_u64;
-    let mut instructions = instructions.iter().cycle();
-    while !cur_nodes.iter().all(|node| node.ends_with('Z')) {
-        num_steps += 1;
-        let cur_instruction = instructions.next().unwrap();
-        if num_steps % 1000000 == 0 {
-            println!("{} - {}: {:?}", cur_instruction, num_steps, cur_nodes);
-        }
-        for cur_node in cur_nodes.iter_mut() {
-            let (left, right) = graph.get(*cur_node).unwrap();
-            *cur_node = match cur_instruction {
+    // In part 2, we have multiple start nodes, so we need to find the number of steps for each
+    // but, the routes loop, so we need to find the LCM of the number of steps for each
+    let mut num_steps_for_start_nodes = Vec::new();
+    for start_node in start_nodes {
+        let mut num_steps = 0_u64;
+        let mut instructions = instructions.iter().cycle();
+        let mut cur_node = start_node;
+        while !cur_node.ends_with('Z') {
+            num_steps += 1;
+            let cur_instruction = instructions.next().unwrap();
+
+            let (left, right) = graph.get(cur_node).unwrap();
+            let next_node = match cur_instruction {
                 'L' => left,
                 'R' => right,
                 _ => panic!("Invalid instruction"),
             };
-        }
 
-        // println!("{}: {:?}", num_steps, cur_nodes);
+            // println!("{}: {:?}", num_steps, cur_nodes);
+            cur_node = next_node;
+        }
+        num_steps_for_start_nodes.push(num_steps);
+        println!("{}: {}", start_node, num_steps);
     }
+
+    let num_steps = lcm_many(&num_steps_for_start_nodes);
 
     println!("Part 2: {}", num_steps);
     if let Some(expected_steps) = expected_steps {
         assert_eq!(expected_steps, num_steps);
     }
+}
+
+fn lcm_many(nums: &[u64]) -> u64 {
+    let mut nums = nums.iter();
+    let mut x = *(nums.next().unwrap());
+    for y in nums {
+        x = lcm(x, *y);
+    }
+    x
+}
+
+fn gcd(a: u64, b: u64) -> u64 {
+    let mut a = a;
+    let mut b = b;
+    while a != b {
+        if a > b {
+            a -= b;
+        } else {
+            b -= a;
+        }
+    }
+    a
+}
+
+fn lcm(a: u64, b: u64) -> u64 {
+    a * b / gcd(a, b)
 }
 
 fn main() {
